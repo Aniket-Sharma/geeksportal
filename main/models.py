285a4495510251e598e2a4f8c2e01c76ab4328c2
Aniket Sharma <abcd13017@gmail.com>
2019-08-26 14:07:14 +0530

    Took care of the emply reply, and forum post bug

--- main/models.py
+++ main/models.py
@@ -5,8 +5,8 @@ from django.db import models
 
 class ForumPost(models.Model):
     sender = models.CharField(max_length=100, primary_key=False)
-    title = models.TextField(null=True, blank=True)
-    post = models.TextField(null=True, blank=True)
+    title = models.TextField()
+    post = models.TextField()
     posted_at = models.DateTimeField(auto_now_add=True)
     up_vote = models.IntegerField(default=0)
     down_vote = models.IntegerField(default=0)
@@ -16,9 +16,9 @@ class ForumPost(models.Model):
 
 
 class ForumPostReply(models.Model):
-    forum_post = models.ForeignKey(ForumPost, on_delete=models.CASCADE, null=True, blank=True)
+    forum_post = models.ForeignKey(ForumPost, on_delete=models.CASCADE)
     user = models.CharField(max_length=100)
-    post = models.TextField(null=True, blank=True)
+    post = models.TextField()
     posted_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
